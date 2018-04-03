b08b1defb76446b1686942f7272b7b9a11a8ebbd
Kris West <kris@chartiq.com>
2018-04-03 11:48:01 +0100

    cleaning up some conflicts and reeneabling yellowFin comp code

--- src/components/yellowFin/yellowFin.js
+++ src/components/yellowFin/yellowFin.js
@@ -41,59 +41,59 @@ FSBL.addEventListener('onReady', function () {
 	let spawnData = null;
 	spawnData = FSBL.Clients.WindowClient.getSpawnData();
 
-	// Logger.log("Spawn data: " + JSON.stringify(spawnData, undefined, 2));
-	// if (spawnData.destination) {
-	// 	destination = spawnData.destination;
-	// 	Logger.log(`Set destination: ${destination}`); 
-	// } else {
-	// 	Logger.log("no destination details found in spawn data - will launch straight to yellowfin dashboard");
-	// }
-	// if (spawnData.server) {
-	// 	serverDetails = spawnData.server;
-	// 	Logger.log(`Set server details: ${JSON.stringify(destination, undefined, 2)}`); 
-	// } else {
-	// 	Logger.log("no server details found in spawn data - will launch to service's default server");
-	// }
+	Logger.log("Spawn data: " + JSON.stringify(spawnData, undefined, 2));
+	if (spawnData.destination) {
+		destination = spawnData.destination;
+		Logger.log(`Set destination: ${destination}`); 
+	} else {
+		Logger.log("no destination details found in spawn data - will launch straight to yellowfin dashboard");
+	}
+	if (spawnData.server) {
+		serverDetails = spawnData.server;
+		Logger.log(`Set server details: ${JSON.stringify(destination, undefined, 2)}`); 
+	} else {
+		Logger.log("no server details found in spawn data - will launch to service's default server");
+	}
 
-	// getServer(function(err, server) {
-	// 	if (err) {
-	// 		Logger.error("Failed to retrieve server details: ", err);
-	// 	} else {
-	// 		serverDetails = server;
-	// 		//Logger.log("serverDetails: " + JSON.stringify(serverDetails, undefined, 2));	
-	// 		FSBL.Clients.WindowClient.setWindowTitle(`YellowFin (${serverDetails.yellowfinHost}:${serverDetails.yellowfinPort})`);
+	getServer(function(err, server) {
+		if (err) {
+			Logger.error("Failed to retrieve server details: ", err);
+		} else {
+			serverDetails = server;
+			//Logger.log("serverDetails: " + JSON.stringify(serverDetails, undefined, 2));	
+			FSBL.Clients.WindowClient.setWindowTitle(`YellowFin (${serverDetails.yellowfinHost}:${serverDetails.yellowfinPort})`);
  
-	// 		setState();
+			setState();
 
-	// 		getLoginToken(serverDetails, function(err, token) {
-	// 			if (err) {
-	// 				//Logger.error("Failed to retreive login token from webservice!");
-	// 			} else {
-	// 				//Logger.log("Retrieved login token: " + token);
+			getLoginToken(serverDetails, function(err, token) {
+				if (err) {
+					//Logger.error("Failed to retreive login token from webservice!");
+				} else {
+					//Logger.log("Retrieved login token: " + token);
 					
-	// 				let yfURL = serverDetails.yellowfinProtocol + serverDetails.yellowfinHost + ":" + serverDetails.yellowfinPort + "/logon.i4?LoginWebserviceId=" + token + "&disablelogoff=true&hideheader=true&hidefooter=true";
-	// 				if (destination) {
-	// 					yfURL += `&entry=${destination}`;
-	// 				}
-	// 				yf_iframe.setAttribute('src', yfURL);
-	// 				yf_iframe.setAttribute('width', window.innerWidth-5);
-	// 				yf_iframe.setAttribute('height', window.innerHeight-50);
+					let yfURL = serverDetails.yellowfinProtocol + serverDetails.yellowfinHost + ":" + serverDetails.yellowfinPort + "/logon.i4?LoginWebserviceId=" + token + "&disablelogoff=true&hideheader=true&hidefooter=true";
+					if (destination) {
+						yfURL += `&entry=${destination}`;
+					}
+					yf_iframe.setAttribute('src', yfURL);
+					yf_iframe.setAttribute('width', window.innerWidth-5);
+					yf_iframe.setAttribute('height', window.innerHeight-50);
 
-	// 				document.body.appendChild(yf_iframe);
-	// 				$('#yf_iframe').height(window.innerHeight-50);
-	// 				$('#yf_iframe').width(window.innerWidth-5);
-	// 			}
-	// 		});
-	// 	}
-	// });
+					document.body.appendChild(yf_iframe);
+					$('#yf_iframe').height(window.innerHeight-50);
+					$('#yf_iframe').width(window.innerWidth-5);
+				}
+			});
+		}
+	});
 
-	// FSBL.Clients.WindowClient.setWindowTitle("YellowFin");
+	FSBL.Clients.WindowClient.setWindowTitle("YellowFin");
 
-	// //resize the iframe on window resize
-	// window.onresize = function() { 
-	// 	$('#yf_iframe').height(window.innerHeight-50);
-	// 	$('#yf_iframe').width(window.innerWidth-5);
-	// };
+	//resize the iframe on window resize
+	window.onresize = function() { 
+		$('#yf_iframe').height(window.innerHeight-50);
+		$('#yf_iframe').width(window.innerWidth-5);
+	};
 
 	
 }); 
