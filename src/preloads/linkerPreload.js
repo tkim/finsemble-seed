53dd449d3bdd9c0ee9862626b4e0e658a4c4f39e
gerard-ciq <gerard@chartiq.com>
2019-05-22 14:37:14 +0100

    Running through seed project documentation Chapters 1 - 3

---
+++ src/preloads/linkerPreload.js
@@ -0,0 +1,7 @@
+const alertMe = () => window.alert("Well, hot dog!")
+    
+if (window.FSBL && FSBL.addEventListener) {
+    FSBL.addEventListener("onReady", alertMe);
+} else {
+    window.addEventListener("FSBLReady", alertMe);
+}
