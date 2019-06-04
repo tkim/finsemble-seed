4fef57197011f56618de5e01ec5cc11a1f0f86a4
Jimmy Stevens <jimmy@chartiq.com>
2019-06-04 14:02:19 -0400

    Update Webpack with correct log rocket name

--- src-built-in/preloads/logRocket.js
+++ src-built-in/preloads/logRocket.js
@@ -3,8 +3,8 @@ LogRocket.init('l9ijht/fs');
 
 // This is an example script - don't forget to change it!
 LogRocket.identify('l9ijht/fs', {
-    name: 'Jimmy',
-    email: 'jimmy@chartiq.com',
+    name: 'CIQTest',
+    email: 'logrocket@chartiq.com',
   
     // Add your own custom user variables here, ie:
     sessionName: 'Lightning Talk Test'
