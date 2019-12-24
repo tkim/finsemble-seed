34652fa4dba8794374263e38244ba8da7c01239d
Kris West <kris@chartiq.com>
2019-12-24 11:42:16 +0000

    Adding ability to start and stop the headless example to tomcat manager, some small visual finesse, removing start/stop scripts from launcher menu

--- src/components/tomcatManager/tomcatManager.js
+++ src/components/tomcatManager/tomcatManager.js
@@ -1,29 +1,27 @@
+
+
 const FSBLReady = () => {
 	try {
-		// Do things with FSBL in here.
-		// const startServiceButton = document.createElement("button");
-		// startServiceButton.appendChild(document.createTextNode("Start Tomcat"));
-		// startServiceButton.onclick = () => FSBL.Clients.LauncherClient.spawn("Start Tomcat Service");
-		// document.body.appendChild(startServiceButton);
+		// ================================
+		// Setup service manager buttons
 
-		// const stopServiceButton = document.createElement("button");
-		// stopServiceButton.appendChild(document.createTextNode("Stop Tomcat Service"));
-		// stopServiceButton.onclick = () => FSBL.Clients.LauncherClient.spawn("Stop Tomcat Service");
-		// document.body.appendChild(stopServiceButton);
-
+		// --------------------------------
+		// Tomcat managed via start/stop script
 		const startScriptButton = document.createElement("button");
+		startScriptButton.className = "button";
 		startScriptButton.appendChild(document.createTextNode("Start Tomcat Script"));
 		startScriptButton.onclick = () => FSBL.Clients.LauncherClient.spawn("Start Tomcat Script");
 		document.body.appendChild(startScriptButton);
 
 		const stopScriptButton = document.createElement("button");
+		stopScriptButton.className = "button";
 		stopScriptButton.appendChild(document.createTextNode("Stop Tomcat Script"));
 		stopScriptButton.onclick = () => FSBL.Clients.LauncherClient.spawn("Stop Tomcat Script");
 		document.body.appendChild(stopScriptButton);
 
 		const tomcatStatus = document.createElement("div");
 		document.body.appendChild(tomcatStatus);
-		tomcatStatus.innerText = "unknown";
+		tomcatStatus.innerText = "status: unknown";
 
 		setInterval(async () => {
 			const data = (await FSBL.Clients.LauncherClient.getActiveDescriptors()).data;
@@ -31,10 +29,82 @@ const FSBLReady = () => {
 			const tomcatStops = Object.keys(data).filter(key => key.startsWith("Stop Tomcat Script"));
 			const isRunning = tomcatStarts.length > tomcatStops.length;
 			const status = isRunning ? "Running" : "Stopped";
-			tomcatStatus.innerText = status;
-		}, 1000)
+			tomcatStatus.innerText = "status: " + status;
+		}, 1000);
+
+
+		// --------------------------------
+		// Headless java component
+		const startHeadlessButton = document.createElement("button");
+		startHeadlessButton.className = "button";
+		startHeadlessButton.appendChild(document.createTextNode("Start Headless java script"));
+		startHeadlessButton.onclick = () => FSBL.Clients.LauncherClient.spawn("commerz_poc_headless");
+		document.body.appendChild(startHeadlessButton);
+
+		const stopHeadlessButton = document.createElement("button");
+		stopHeadlessButton.className = "button";
+		stopHeadlessButton.appendChild(document.createTextNode("Stop all Headless java Scripts"));
+		stopHeadlessButton.onclick = () => {
+			closeComponents("commerz_poc_headless");
+		};
+		document.body.appendChild(stopHeadlessButton);
+
+		const headlessStatus = document.createElement("div");
+		document.body.appendChild(headlessStatus);
+		headlessStatus.innerText = "status: unknown";
+
+		setInterval(async () => {
+			findInstances("commerz_poc_headless")
+			.then(function(windowIdentifiers){
+				const numRunning = windowIdentifiers.length;
+				const status = numRunning > 0 ? numRunning + " running" : "Stopped";
+				headlessStatus.innerText = "status: " + status;
+			})
+			.catch(function(err){
+				console.error(err)
+			});
+			
+		}, 1000);
+
 	} catch (e) {
 		FSBL.Clients.Logger.error(e);
+	}
+} 
+
+/**
+	Example function to close all instances of a component
+*/
+function closeComponents(componentType) {
+	findInstances(componentType)
+	.then(function(windowIdentifiers){
+		windowIdentifiers.forEach(element => {
+			FSBL.FinsembleWindow.getInstance(element)
+			.then(({wrap}) => { wrap.close({removeFromWorkspace:true});} )
+			.catch(function(err){
+				console.error(err);
+			});
+		}); 
+	})
+	.catch(function(err){
+		console.error(err)
+	});
+}
+
+/** Example Function to check if one or more instances of a component currently exist and to return 
+windowIdentifiers to allow you to make calls relating to them. */
+async function findInstances(componentType){
+	let {err, data} = await FSBL.Clients.LauncherClient.getActiveDescriptors();
+	if (err) {
+		console.error(err);
+		return Promise.reject(err);
+	} else {
+		let windowIdentifiers = [];
+		Object.keys(data).forEach(windowName => {
+			if(data[windowName].componentType == componentType) {
+				windowIdentifiers.push({componentType: componentType, windowName: windowName});
+			}
+		});
+		return Promise.resolve(windowIdentifiers);
 	}
 }
 
