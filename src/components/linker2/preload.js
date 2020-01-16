3c1fb629d130709c6bcc5c636a5bd6d1d8913755
Gerard Petersen <gerard@chartiq.com>
2020-01-16 18:26:58 +0000

    removing id work

--- src/components/linker2/preload.js
+++ src/components/linker2/preload.js
@@ -8,10 +8,10 @@ if (window.FSBL && FSBL.addEventListener) {
 async function init() {
 	const messages = {};
 	FSBL.Clients.LinkerClient.subscribe("symbol", (symbol, response) => {
-		if (!messages.hasOwnProperty(response.data.message_id)) {
-			messages[response.data.message_id] = true;
+		// if (!messages.hasOwnProperty(response.data.message_id)) {
+		// 	messages[response.data.message_id] = true;
 			console.log(response);
 			// process message
-		}
+		// }
 	});
 }
