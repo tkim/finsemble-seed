eb326e62d86a3123cc637ad03f321f41badb2286
Kris West <kris@chartiq.com>
2020-01-17 11:37:49 +0000

    add some more console output for debugging

--- src/components/linker2/preload.js
+++ src/components/linker2/preload.js
@@ -7,10 +7,16 @@ if (window.FSBL && FSBL.addEventListener) {
 
 async function init() {
 	const messages = {};
+	const subscribedAt = new Date().toISOString();
+	const rand = Math.random();
+	let timesCalled = 0;
 	FSBL.Clients.LinkerClient.subscribe("symbol", (symbol, response) => {
 		// if (!messages.hasOwnProperty(response.data.message_id)) {
 		// 	messages[response.data.message_id] = true;
-			console.log(response);
+			console.log("Subscription handler applied at: " + subscribedAt + 
+				"\nRandom number: " + rand + 
+				"\ntimes called: " + timesCalled++ + 
+				"\n, response:", response);
 			// process message
 		// }
 	});
