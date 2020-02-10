b2c6a9aa6ecfb37c3e7419a4d6140b324062692d
WatsonCIQ <chris.watson@chartiq.com>
2020-02-10 08:15:58 +0000

    working seed

--- src-built-in/components/toolbar/components/BringToFront.jsx
+++ src-built-in/components/toolbar/components/BringToFront.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 import { FinsembleButton } from "@chartiq/finsemble-react-controls";
-import { ReactComponent as BringToFrontIcon } from '../../../../assets/img/toolbar/bring-to-front.svg'
-
+import { ReactComponent as BringToFrontIcon } from "../../../../assets/img/toolbar/bring-to-front.svg";
+import NotificationIcon from "../../../../src/components/finsemble-notifications/components/notification-icon/NotificationIcon";
 // Store
 import ToolbarStore from "../stores/toolbarStore";
 
-
-const BringToFront = (props) => {
+const BringToFront = props => {
 	const BringToFront = () => {
 		FSBL.Clients.LauncherClient.bringWindowsToFront({}, () => {
 			ToolbarStore.bringToolbarToFront();
 		});
-	}
+	};
 	let wrapperClasses = props.classes + " icon-only window-mgmt-right";
 
 	return (
-		<FinsembleButton className={wrapperClasses} buttonType={["Toolbar"]} title="Reveal All" onClick={BringToFront}>
-			<span>
-				<BringToFrontIcon />
-			</span>
-		</FinsembleButton>
-	)
+		<div>
+			{/* <NotificationIcon /> */}
+			<FinsembleButton
+				className={wrapperClasses}
+				buttonType={["Toolbar"]}
+				title="Reveal All"
+				onClick={BringToFront}
+			>
+				<span>
+					<BringToFrontIcon />
+				</span>
+			</FinsembleButton>
+		</div>
+	);
 };
 
 export default BringToFront;
