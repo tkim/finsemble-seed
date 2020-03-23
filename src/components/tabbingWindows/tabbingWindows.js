675ae9e9902bffaf48d00d79065579adb12dcb97
Kris West <kris@chartiq.com>
2020-03-23 10:25:31 +0000

    whitespace formatting

--- src/components/tabbingWindows/tabbingWindows.js
+++ src/components/tabbingWindows/tabbingWindows.js
@@ -5,33 +5,33 @@ let i = -1;
 const addChildWindow = async () => {
 	if (!firstChild) {
 		// Create the first window below the parent window and group the two windows together.
-        console.log("*** Creating first child");
-        const spawnParams = {
-            groupOnSpawn: true,
-            top: "adjacent",
-            position: "relative"
-        };
-        firstChild = (await FSBL.Clients.LauncherClient.spawn(componentType, spawnParams)).response;
-        console.log("*** First child created");
+		console.log("*** Creating first child");
+		const spawnParams = {
+			groupOnSpawn: true,
+			top: "adjacent",
+			position: "relative"
+		};
+		firstChild = (await FSBL.Clients.LauncherClient.spawn(componentType, spawnParams)).response;
+		console.log("*** First child created");
 	} else if (!stackedWindow) {
 		// Create the second window in the same location as the first window so it doesn't spawn in one location then
 		// move to another. 
-        console.log("*** Creating second child");
-        const spawnParams = {
-            top: "adjacent",
-            position: "relative"
-        };
-        const secondChild = (await FSBL.Clients.LauncherClient.spawn(componentType, spawnParams)).response;
-		
+		console.log("*** Creating second child");
+		const spawnParams = {
+			top: "adjacent",
+			position: "relative"
+		};
+		const secondChild = (await FSBL.Clients.LauncherClient.spawn(componentType, spawnParams)).response;
+
 		// Create the StackedWindow to contain the windows that are tabbed together.
 		console.log("*** Creating stacked window", stackedWindowParams);
 		const stackedWindowParams = {
-            windowIdentifiers: [
-                firstChild.windowIdentifier,
-                secondChild.windowIdentifier
-            ],
-            visibleWindowIdentifier: secondChild.windowIdentifier,
-            create: true
+			windowIdentifiers: [
+				firstChild.windowIdentifier,
+				secondChild.windowIdentifier
+			],
+			visibleWindowIdentifier: secondChild.windowIdentifier,
+			create: true
 		};
 
 		// Listen for the "parent-set" event to get a handle to the stacked window.
@@ -54,30 +54,30 @@ const addChildWindow = async () => {
 				}
 			);
 			secondChild.finWindow.removeListener("parent-set", onParentSet);
-			
+
 			// Set the window that is visible in the StackedWindow.
 			stackedWindow.setVisibleWindow({ windowIdentifier: secondChild.windowIdentifier })
 		};
 		secondChild.finWindow.addListener("parent-set", onParentSet);
 
 		FSBL.Clients.WindowClient.getStackedWindow(stackedWindowParams);
-    } else {
+	} else {
 		// Create a child window in the same location as the first window so it doesn't spawn in one location then move 
 		// to another. 
-        console.log("*** Adding to stacked window");
-        const spawnParams = {
-            top: "adjacent",
-            position: "relative"
+		console.log("*** Adding to stacked window");
+		const spawnParams = {
+			top: "adjacent",
+			position: "relative"
 		};
-        child = (await FSBL.Clients.LauncherClient.spawn(componentType, spawnParams)).response;
-		
+		child = (await FSBL.Clients.LauncherClient.spawn(componentType, spawnParams)).response;
+
 		// Add the created child window to the StackedWindow.
 		stackedWindow.addWindow({ windowIdentifier: child.windowIdentifier, position: i }, () => {
 			stackedWindow.setVisibleWindow({ windowIdentifier: child.windowIdentifier })
 		});
-        console.log("** Window added to stacked window");
-    }
-    i++;
+		console.log("** Window added to stacked window");
+	}
+	i++;
 }
 
 const FSBLReady = () => {
