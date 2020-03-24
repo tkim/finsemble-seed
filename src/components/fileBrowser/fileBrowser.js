a536fe0365fd7bca57093181f836044f3d1288d2
NinjaManatee <andy@chartiq.com>
2020-03-24 12:35:15 -0400

    Added CSS styling to the form Added pass of currently selected folder to Windowless.SelectFolder Input validation Added labels to the form

--- src/components/fileBrowser/fileBrowser.js
+++ src/components/fileBrowser/fileBrowser.js
@@ -1,13 +1,13 @@
 const fileBrowse = () => {
 	// TODO: Make the query be able to take an initial directory
+	const selectedFolder = document.getElementById("selectedFolder");
 	const params = {
-		//initialDirectory: ""
+		selectedFolder: selectedFolder.value
 	};
 	FSBL.Clients.RouterClient.query("Windowless.SelectDirectory", params, (err, res) => { 
 		if (err) {
 			alert(err);
 		} else {
-			const selectedFolder = document.getElementById("selectedFolder");
 			selectedFolder.value = res.data.selectedPath;
 		}
 	});
@@ -15,6 +15,12 @@ const fileBrowse = () => {
 
 const listFolderContents = () => {
 	const selectedFolder = document.getElementById("selectedFolder");
+
+	if (selectedFolder.value === "") {
+		alert("Folder must be selected before listing contents");
+		return;
+	}
+
 	const params = {
 		folder: selectedFolder.value
 	};
@@ -36,8 +42,17 @@ const listFolderContents = () => {
 
 const writeFile = () => {
 	const folder = document.getElementById("selectedFolder");
-	const filename = document.getElementById("fileName");
+	const filename = document.getElementById("filename");
 	const contents = document.getElementById("fileContents");
+
+	if (filename.value === "") {
+		alert("Filename must be specified to save a file");
+		return;
+	} else if (folder.value === "") {
+		alert("Selected Folder must be specified to save a file.");
+		return;
+	}
+
 	const filePath = `${folder.value}\\${filename.value}`;
 	const params = {
 		filename: filePath,
@@ -49,48 +64,82 @@ const writeFile = () => {
 const FSBLReady = () => {
 	try {
 		// Do things with FSBL in here.
+		const container = document.createElement("div");
+		container.id = "container";
+		document.body.appendChild(container);
+
+		const title = document.createElement("h1");
+		title.appendChild(document.createTextNode("File Browser"));
+		container.appendChild(title);
+
 		const fileBrowseButton = document.createElement("button");
 		fileBrowseButton.appendChild(document.createTextNode("Browser for Folder"));
 		fileBrowseButton.onclick = fileBrowse;
-		document.body.appendChild(fileBrowseButton);
+		container.appendChild(fileBrowseButton);
 
-		document.body.appendChild(document.createElement("br"));
+		let spacer = document.createElement("div");
+		spacer.className = "spacer2";
+		container.appendChild(spacer);
+
+		const selectedFolderLabel = document.createElement("label");
+		selectedFolderLabel.for = "selectedFolder";
+		selectedFolderLabel.appendChild(document.createTextNode("Selected Folder"));
+		container.appendChild(selectedFolderLabel);
 
 		const folderTextField = document.createElement("input");
 		folderTextField.id = "selectedFolder";
-		document.body.appendChild(folderTextField);
+		folderTextField.readOnly = true;
+		container.appendChild(folderTextField);
 
-		document.body.appendChild(document.createElement("br"));
+		spacer = document.createElement("div");
+		spacer.className = "spacer2";
+		container.appendChild(spacer);
 
 		const listFolderContentsButton = document.createElement("button");
 		listFolderContentsButton.appendChild(document.createTextNode("List Folder Contents"));
 		listFolderContentsButton.onclick = listFolderContents;
-		document.body.appendChild(listFolderContentsButton);
+		container.appendChild(listFolderContentsButton);
 
-		document.body.appendChild(document.createElement("br"));
+		container.appendChild(document.createElement("br"));
+
+		const folderListingLabel = document.createElement("label");
+		folderListingLabel.for = "folderListing";
+		folderListingLabel.appendChild(document.createTextNode("Folder Listing"));
+		container.appendChild(folderListingLabel);
 
 		const folderListing = document.createElement("ul");
 		folderListing.id = "folderListing";
-		document.body.appendChild(folderListing);
+		container.appendChild(folderListing);
 
-		document.body.appendChild(document.createElement("br"));
+		container.appendChild(document.createElement("br"));
 
-		const fileNameTextField = document.createElement("input");
-		fileNameTextField.id = "fileName";
-		document.body.appendChild(fileNameTextField);
+		const filenameLabel = document.createElement("label");
+		filenameLabel.for = "filename"
+		filenameLabel.appendChild(document.createTextNode("Filename"));
+		container.appendChild(filenameLabel);
 
-		document.body.appendChild(document.createElement("br"));
+		const filenameTextField = document.createElement("input");
+		filenameTextField.id = "filename";
+		filenameTextField.defaultValue = "test.txt";
+		container.appendChild(filenameTextField);
+
+		container.appendChild(document.createElement("br"));
+
+		const fileContentsLabel = document.createElement("label");
+		fileContentsLabel.for = "fileContents"
+		fileContentsLabel.appendChild(document.createTextNode("File Contents"));
+		container.appendChild(fileContentsLabel);
 
 		const fileContentsField = document.createElement("textarea");
 		fileContentsField.id = "fileContents";
-		document.body.appendChild(fileContentsField);
+		container.appendChild(fileContentsField);
 
-		document.body.appendChild(document.createElement("br"));
+		container.appendChild(document.createElement("br"));
 
 		const writeFileButton = document.createElement("button");
 		writeFileButton.appendChild(document.createTextNode("Write File"));
 		writeFileButton.onclick = writeFile;
-		document.body.appendChild(writeFileButton);
+		container.appendChild(writeFileButton);
 
 	} catch (e) {
 		FSBL.Clients.Logger.error(e);
