ee43697e2035adfd6cf1510d24b67574407be94f
Kris West <kris@chartiq.com>
2020-05-19 10:40:53 +0100

    buff recipe comments

--- src/services/callRequestListener/callRequestListenerService.js
+++ src/services/callRequestListener/callRequestListenerService.js
@@ -3,11 +3,25 @@ const Finsemble = require("@chartiq/finsemble");
 Finsemble.Clients.Logger.start();
 Finsemble.Clients.Logger.log("callRequestListener Service starting up");
 
+// Add and initialize any other clients you need to use (services are initialized by the system, clients are not):
+// Finsemble.Clients.AuthenticationClient.initialize();
+// Finsemble.Clients.ConfigClient.initialize();
+// Finsemble.Clients.DialogManager.initialize();
+// Finsemble.Clients.DistributedStoreClient.initialize();
+// Finsemble.Clients.DragAndDropClient.initialize();
 Finsemble.Clients.LauncherClient.initialize();
+// Finsemble.Clients.LinkerClient.initialize();
+// Finsemble.Clients.HotkeyClient.initialize();
+// Finsemble.Clients.SearchClient.initialize();
+// Finsemble.Clients.StorageClient.initialize();
+// Finsemble.Clients.WindowClient.initialize();
+// Finsemble.Clients.WorkspaceClient.initialize();
+
 // NOTE: When adding the above clients to a service, be sure to add them to the start up dependencies.
 
-
+//Ensure an instant of this component has been launched and is brought to front when starting a call 
 const COMPONENT_TYPE = "Welcome Component";
+//response code indicating call was started successfully
 const RESPONSE_CODE_SUCCESS = 0
 
 
@@ -44,19 +58,31 @@ class callRequestListenerService extends Finsemble.baseService {
 		callback();
 	}
 
+	/**
+	 * Ensure a particular component is on the screen, initiate a call and if successful,
+	 * broadcast data about the call for other components to respond to as context.
+	 * @param {*} data 
+	 */
 	makeCall(data) {
 		return new Promise(async (resolve) => {
+			//Ensure a particular component is open
 			await this.ensureOpen();
+			//Attempt to initiate the call
 			let responseCode = await this.sendRequest(data);
 			if(responseCode === RESPONSE_CODE_SUCCESS) {
-				// Notify components that can provide context for the call
+				// Notify other components by broadcasting context data about the call
 				this.broadcastCall(data);
 			}
 			resolve(responseCode);
 		})
 	}
 
+	/**
+	 * Broadcast context data about the call.
+	 * @param {*} data 
+	 */
 	broadcastCall(data) {
+		//TODO: Implement your context broadcast here, for example with an FDC3 desktop agent or the Finsemble ROuterClient
 		Finsemble.Clients.RouterClient.transmit("callRequestListener.callStarted", data);
 	}
 
@@ -64,9 +90,9 @@ class callRequestListenerService extends Finsemble.baseService {
 	 * @param data
 	 */
 	sendRequest(data) {
-		// Make http request to Cloud 9 service as you normally wyould
+		//TODO: Make a request to Cloud 9 service to initiate the call
 
-		// Alternatively Send request via Router
+		// E.g. via the Finsemble RouterClient
 		// return new Promise((resolve, reject) => {
 		// 	Finsemble.Clients.RouterClient.query(
 		// 		"C9.channel.if.finsemble",
@@ -92,7 +118,7 @@ class callRequestListenerService extends Finsemble.baseService {
 	 * Add query responders, listeners or pub/sub topic as appropriate.
 	 */
 	createRouterEndpoints() {
-		// Add responder for myFunction
+		// Add query responder for initiating a call
 		Finsemble.Clients.RouterClient.addResponder("callRequestListener.makeCall", (err, message) => {
 			if (err) {
 				return Finsemble.Clients.Logger.error("Failed to setup callRequestListener.makeCall responder", err);
@@ -101,12 +127,13 @@ class callRequestListenerService extends Finsemble.baseService {
 			Finsemble.Clients.Logger.log('callRequestListener Query: ' + JSON.stringify(message));
 
 			try {
+				//TODO: add any data validation here and raise an exception if necessary
+				
 				// Data in query message can be passed as parameters to a method in the service.
 				this.makeCall(message.data).then(responseCode => {
+					// Send query response to the function call, with optional data, back to the caller.
 					message.sendQueryResponse(null, responseCode);
 				});
-
-				// Send query response to the function call, with optional data, back to the caller.
 			} catch (e) {
 				// If there is an error, send it back to the caller
 				message.sendQueryResponse(e);
@@ -114,9 +141,13 @@ class callRequestListenerService extends Finsemble.baseService {
 		});
 	}
 
+	/**
+	 * Ensure that an instance of nominated component is open, if so bring it to front, or spawn it if not
+	 */
 	ensureOpen() {
 		return new Promise(async (resolve, reject) => {
 			// Show the UI if it's up already or spawn it
+			  //Additional arguments may be added to the second parameter if you also wish to reposition the component
 			let {err, response} = await Finsemble.Clients.LauncherClient.showWindow(
 				{"componentType": COMPONENT_TYPE},
 				{"spawnIfNotFound": true}
