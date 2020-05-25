9976a10a22f0c4d2656a407c098a70b0550d5e91
thorsent <terry@chartiq.com>
2020-05-25 09:33:05 -0400

    Formatting

--- src/components/toolbar/src/FileMenu.jsx
+++ src/components/toolbar/src/FileMenu.jsx
@@ -9,13 +9,13 @@ export const FileMenu = () => {
 				<img className="finsemble-toolbar-brand-logo" src="../../../assets/img/Finsemble_Taskbar_Icon.png" />
 			</MenuToggle>
 			<Menu>
-				<Preferences/>
-				<SystemLog/>
-				<CentralLogger/>
-				<Documentation/>
+				<Preferences />
+				<SystemLog />
+				<CentralLogger />
+				<Documentation />
 				<Restart />
-				<Reset/>
-				<Quit/>
+				<Reset />
+				<Quit />
 				{/* To add your own items to the menu, import MenuItem from
 				  * "@chartiq/finsemble-ui/react/components" and add the following:
 				  * <MenuItem onClick={...}>Your Item</MenuItem>
