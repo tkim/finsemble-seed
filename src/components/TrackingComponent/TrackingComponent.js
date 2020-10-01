01f40868ead2404dca215c4345e41f9312e59fe5
Gerard Petersen <gerard@chartiq.com>
2020-10-01 17:36:53 +0100

    Added linker functionality to tracking

--- src/components/TrackingComponent/TrackingComponent.js
+++ src/components/TrackingComponent/TrackingComponent.js
@@ -2,8 +2,21 @@ var componentToTrackName
 var componentToTrack
 var currentBounds
 
+var trackingStore
+
 const FSBLReady = async () => {
+	document.getElementById("backup").onclick = persistTrackedState;
+	document.getElementById("restore").onclick = restoreTrackedState;
+
 	try {
+		FSBL.Clients.DistributedStoreClient.createStore(
+			{ store: `tracking-state-${finsembleWindow.identifier.windowName}`, global: true, persist:true, values: {} },
+			(err, storeObject) => {
+				trackingStore = storeObject;
+			}
+		);
+
+
 		FSBL.Clients.WindowClient.getBounds(async (err, bound) => {
 			let spawnData = FSBL.Clients.WindowClient.getSpawnData();
 			componentToTrackName = spawnData.componentToTrack
@@ -33,7 +46,7 @@ const moveComponentToTrackToMousePos = () => {
 	})
 }
 
-constgetMousePosition = (cb) => {
+const getMousePosition = (cb) => {
 	fin.desktop.System.getMousePosition((mousePosition) => {
 		if (mousePosition.left || mousePosition.left === 0)
 			mousePosition.x = mousePosition.left;
@@ -59,6 +72,7 @@ const restoreComponentToTrack = async (bound) => {
 	}, async (err, windowIdentifier) => {
 		componentToTrack = windowIdentifier.finWindow
 		componentToTrack.restore(() => {
+			restoreTrackedState();
 			componentToTrack.focus()
 		})
 	})
@@ -114,7 +128,45 @@ const setTrackingComponentPosition = async (position) => {
 	})
 }
 
+const restoreTrackedState = async () => {
+		let allChannels = FSBL.Clients.LinkerClient.getAllChannels();
+
+			allChannels.forEach((channel) => {
+				try {
+					console.log(`unlnking ${channel}`);
+					FSBL.Clients.LinkerClient.unlinkFromChannel(channel.name, componentToTrack.identifier);
+				} catch (e) {
+					console.log(e);
+				}
+			})
+
+		trackingStore.getValue('channels', (err, channels) => {
+			if(!err && channels) {
+				channels.forEach((channelName) => {
+					console.log(`linking ${channelName}`);
+					// FSBL.Clients.LinkerClient.linkToChannel(channelName, componentToTrack.identifier)
+				})
+			}
+		});
+}
+const persistTrackedState = async () => {
+	let components = await FSBL.Clients.LinkerClient.getLinkedComponents({
+		componentTypes: [componentToTrackName]
+	});
+
+	let channels = [];
+
+	if(components && components.length) {
+		channels = components[0].channels;
+	}
+
+	console.log(channels);
+
+	trackingStore.setValue({field: "channels", value: channels});
+}
+
 const onunload = () => {
+	persistTrackedState();
 	componentToTrack.minimize()
 }
 
@@ -125,3 +177,26 @@ if (window.FSBL && FSBL.addEventListener) {
 }
 
 window.addEventListener("unload", onunload)
+
+FSBL.Clients.LauncherClient.getActiveDescriptors((err, data) => {
+	let keys = Object.keys(data);
+
+	keys.forEach((key) => {
+		if(key.includes("Welcome")) {
+			console.log(data[key])
+			FSBL.FinsembleWindow.wrap(data[key], (err, wrap) => {
+				let id  = wrap.identifier;
+				delete id["uuid"];
+				delete id["windowType"];
+				delete id["name"];
+				// id["uuid"] = "xyz";
+				id["componentType"] = "Welcome Component";
+				console.log(id)
+				// FSBL.Clients.LauncherClient.showWindow(id, null);
+				FSBL.Clients.LinkerClient.linkToChannel("group4", id);
+			});
+		}
+	})
+});
+
+FSBL.Clients.LinkerClient.getLinkedComponents();
